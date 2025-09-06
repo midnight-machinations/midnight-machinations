92acaf7c95d1dc1378b9c7fce592f8f491b64d25
Jack Papel <jackpapel@live.com>
2025-09-05 23:55:41 -0400

    Pluralization

--- client/src/game/lang.tsx
+++ client/src/game/lang.tsx
@@ -1,3 +1,4 @@
+import { Parser, Expression } from "expr-eval"
 
 export let langMap: ReadonlyMap<string, string>;
 export let langText: string;
@@ -39,14 +40,30 @@ export function translateAny(langKeys: string[], ...valuesList: (string | number
 }
 
 export function translateChecked(langKey: string, ...valuesList: (string | number)[]): string | null {
+    if (langMap.get(langKey + ':cases') !== undefined) {
+        // These expensive calculations are only run when things have special cases
+        for (const [caseKey, caseValue] of 
+            [...langMap.entries()].filter(([key]) => key.startsWith(langKey + ":case:"))
+        ) {
+            const caseExpr = caseKey.substring((langKey + ":case:").length);
+            if (Parser.parse(populateValues(caseExpr, valuesList)).evaluate()) {
+                return populateValues(caseValue, valuesList);
+            }
+        }
+    }
+
     let out = langMap.get(langKey);
     if(out===undefined){
         return null;
     }
-    for(let i = 0; i < valuesList.length; i++){
-        out = out.replace("\\"+(i), valuesList[i] as string);
+    return populateValues(out, valuesList);
+}
+
+function populateValues(text: string, valuesList: (string | number)[]): string {
+    for (let i = 0; i < valuesList.length; i++) {
+        text = text.replace("\\" + i, valuesList[i] as string);
     }
-    return out;
+    return text;
 }
 
 export function languageName(language: Language): string {
