9eb061c095eb3eef1e404b20243b69b375092ec9
sammy <sammypet3@gmail.com>
2026-01-12 00:29:59 +0000

    add employer

---
+++ server/src/game/role/employer.rs
@@ -0,0 +1,59 @@
+use serde::Serialize;
+use crate::game::prelude::*;
+
+
+#[derive(Clone, Debug, Default)]
+pub struct Employer;
+#[derive(Clone, Debug, Serialize)]
+pub struct ClientRoleState;
+impl GetClientAbilityState<ClientRoleState> for Employer {
+    fn get_client_ability_state(self, _game: &Game, _actor_ref: PlayerReference) -> ClientRoleState {
+        ClientRoleState
+    }
+}
+
+
+pub(super) const MAXIMUM_COUNT: Option<u8> = None;
+pub(super) const DEFENSE: DefensePower = DefensePower::None;
+
+impl RoleStateTrait for Employer {
+    type ClientAbilityState = ClientRoleState;
+    fn on_midnight(self, game: &mut Game, _id: &AbilityID, actor_ref: PlayerReference, midnight_variables: &mut OnMidnightFold, priority: OnMidnightPriority) {
+        if priority != OnMidnightPriority::Convert {return;}
+        let Some(target) = Visits::default_target(midnight_variables, actor_ref, Role::Employer) else {return};
+        let Some(&role) = ControllerID::role(actor_ref, Role::Employer, 1).get_role_list_selection_first(game) else {return};
+
+        if RoleSet::TownCommon.get_roles().contains(&target.role(game)) {
+            target.set_night_convert_role_to(midnight_variables, Some(role.new_state(game)));
+        }else{
+            let role_state = role.new_state(game);
+            AbilityID::Role { role, player: target }.new_role_ability(game, role_state)
+        }
+        // actor_ref.edit_role_ability_helper(game, Employer);
+    }
+    fn controller_parameters_map(self, game: &Game, actor_ref: PlayerReference) -> super::ControllerParametersMap {
+        ControllerParametersMap::combine([
+            //player to convert
+            ControllerParametersMap::builder(game)
+                .id(ControllerID::role(actor_ref, Role::Employer, 0))
+                .single_player_selection_typical(actor_ref, false, true)
+                .night_typical(actor_ref)
+                .build_map(),
+            //role
+            ControllerParametersMap::builder(game)
+                .id(ControllerID::role(actor_ref, Role::Employer, 1))
+                .single_role_selection_typical(game, |r|RoleSet::TownCommon.get_roles().contains(r))
+                .night_typical(actor_ref)
+                .build_map()
+        ])
+    }
+    fn convert_selection_to_visits(self, game: &Game, actor_ref: PlayerReference) -> Vec<Visit> {
+        if ControllerID::role(actor_ref, Role::Employer, 1).get_role_list_selection_first(game).is_none() {return Vec::new()}
+        crate::game::role::common_role::convert_controller_selection_to_visits(
+            game,
+            actor_ref,
+            ControllerID::role(actor_ref, Role::Employer, 0),
+            false,
+        )
+    }
+}
